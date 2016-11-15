9161724e46ee5fe6fbc497e8392095cb586c4d4f
Denis Dvorkovyy <DenisD12@users.noreply.github.com>
2016-11-15 21:49:40 +0100

    Add files via upload

--- main2.js
+++ main2.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
             parallaxBg.css('background-position', '0 ' + -currScrollPos / 4 + 'px');
         });
     })();
+
+    $("nav ul li").on('click', function() {
+      $("li.active2").removeClass('active2');
+      $(this).addClass('active2');
+
+      var boxToShow = $(this).attr('rel');
+
+      $('.active').fadeOut(500, showNextbox);
+
+      function showNextbox() {
+        $(this).removeClass('active');
+
+        $("#" + boxToShow).fadeIn(500, function() {
+          $(this).addClass('active');
+        });
+      }
+    });
+
 });
